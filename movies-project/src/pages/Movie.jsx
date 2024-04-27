14212d2aeed9de864e60a9aad02ac96e77adea7c
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-04-26 22:18:59 -0300

    finished project

--- movies-project/src/pages/Movie.jsx
+++ movies-project/src/pages/Movie.jsx
@@ -16,7 +16,7 @@ const apiKey = import.meta.env.VITE_API_KEY
 
 const Movie = () => {
   const {id} = useParams()
-  const {movie, setMovie} = useState(null)
+  const [movie, setMovie] = useState(null)
 
   const getMovie = async (url) => {
     const res = await fetch(url) 
@@ -24,12 +24,60 @@ const Movie = () => {
     setMovie(data)
   }
 
+  const formatCurrency = (number) => {
+    return number.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    })
+  }
+
   useEffect(() => {
     const movieURL = `${moviesURL}${id}?${apiKey}`
     getMovie(movieURL)
   }, [])
 
-    return <div>{movie && <>{movie.title}</>}</div>
-  }
+    return (
+    <div className="movie-page">
+      {movie && (
+      <>
+        <MovieCard movie={movie} showLink={false} />
+        <p className="tagline">{movie.tagline}</p>
+        <div className="info">
+          <h3>
+            <BsWallet2 /> Orçamento:
+          </h3>
+          <h3>
+            <p>{formatCurrency(movie.budget)}</p>
+          </h3>
+        </div>
+        <div className="info">
+          <h3>
+            <BsGraphUp /> Receita:
+          </h3>
+          <h3>
+            <p>{formatCurrency(movie.revenue)}</p>
+          </h3>
+        </div>
+        <div className="info">
+          <h3>
+            <BsHourglassSplit /> Duração:
+          </h3>
+          <h3>
+            <p>{movie.runtime} minutos</p>
+          </h3>
+        </div>
+        <div className="info description">
+          <h3>
+            <BsFillFileEarmarkTextFill /> Descrição:
+          </h3>
+          <h3>
+            <p>{movie.overview} minutos</p>
+          </h3>
+        </div>
+      </>
+      )}
+    </div>
+  )
+}
   
   export default Movie
